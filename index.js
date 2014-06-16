3c155ff70e10bcc927a5674efcbbf2e2e8a96dac
lixiaojun <cn.lixiaojun@gmail.com>
2014-06-16 11:46:35 +0800

    init repo

---
+++ index.js
@@ -0,0 +1,69 @@
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('koa-version');
+var compose = require('koa-compose');
+var assert = require('assert');
+
+var versionRegExp = new RegExp('^\d.*\d$', 'i');
+
+/**
+ * export `version()`
+ */
+
+module.exports = version;
+
+/**
+ * Mount `app` by `version`, `app`
+ * may be a Koa application or
+ * middleware function.
+ *
+ * @param {String} version in accept header
+ * @param {Application|Function} [app or function]
+ * @return {Function}
+ * @api public
+ */
+
+function version(version, app) {
+  if (typeof version !== 'string') {
+    version = '1';
+  }
+
+  assert(!versionRegExp.test(version), 'Mount version should like `0, 0.1, 1.0.1`');
+
+  // compose
+  var downstream = app.middleware
+    ? compose(app.middleware)
+    : app;
+
+  var name = app.name || 'unnamed';
+
+  return function* (upstream) {
+    if (!this.acceptVersion) {
+      var accept = this.header.accept;
+      this.acceptVersion = _parseAcceptVersion(accept) || '1';
+    }
+
+    if (version !== this.acceptVersion) {
+      return yield* upstream;
+    }
+
+    debug('mount -> %s %s', this.acceptVersion, name);
+    yield* downstream.call(this, function *() {
+      yield* upstream;
+    }.call(this));
+  }
+
+  function _parseAcceptVersion(accept) {
+    var version;
+    try {
+      version = accept.match(/version=(\d+(?:\.\d+)*)/i);
+      version = version[1];
+    } catch (err) {
+      debug('parse accept version failed >>> %s', accept);
+    }
+    return version;
+  };
+
+}
---
+++ test/version.js
@@ -0,0 +1,51 @@
+var request = require('supertest');
+var version = require('..');
+var koa = require('koa');
+
+describe('version(version, app)', function () {
+  it('should mount the app at the given version', function (done) {
+    var app = koa();
+    var a = koa();
+    var b = koa();
+
+    a.use(function* (next) {
+      yield next;
+      this.body = 'Hello';
+    });
+
+    b.use(function* (next) {
+      yield next;
+      this.body = 'World';
+    });
+
+    app.use(version('1', a));
+    app.use(version('2', b));
+
+    request(app.listen())
+      .get('/')
+      .expect('Hello')
+      .end(function (err) {
+        if (err) return done(err);
+        request(app.listen())
+          .get('/')
+          .set('Accept', 'application/json; version=1')
+          .expect('Hello')
+          .end(function (err){
+            if (err) return done(err);
+
+            request(app.listen())
+              .get('/')
+              .set('Accept', 'application/json; version=2')
+              .expect('World')
+              .end(function (err){
+                if (err) return done(err);
+
+                request(app.listen())
+                  .get('/')
+                  .set('Accept', 'application/json; version=3')
+                  .expect(404, done);
+              });
+          });
+      });
+  });
+});
